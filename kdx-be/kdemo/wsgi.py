9e3cc4c226b5027e3d1aaa1a5c96b196960be6e2
kongklw <18261663365@163.com>
2026-05-13 10:47:13 +0800

    remove .idea

--- kdx-be/kdemo/wsgi.py
+++ kdx-be/kdemo/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+print('hhaha')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kdemo.settings')
 
 application = get_wsgi_application()
